5f732a01ac8411a108bc0aae6e8ed10bd3abcdd4
Sundaran V <sundaranv@Sundarans-MacBook-Pro.local>
2026-04-27 11:28:29 +0530

    added base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://asp-mythy-api-prod-uaen.azurewebsites.net/api/v1';
 
 // Create axial instance
 const api = axios.create({
